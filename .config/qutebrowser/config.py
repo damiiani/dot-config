bcec5904e9c13af47c99d6bccabea6105b45d233
damiiani <gustavodamiiani@gmail.com>
2020-05-12 21:11:53 -0300

    Atualizado configurações do qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -28,12 +28,19 @@ c.bindings.commands["normal"] = {
     "q": "tab-close"
 }
 
+c.content.autoplay = False
+
 c.completion.web_history.max_items = 0
+c.confirm_quit = ["always"]
+
+c.content.cookies.store = False
+c.content.headers.accept_language = "pt-BR,pt;q=0.9"
+c.content.pdfjs = True
+c.content.private_browsing = True
+
+c.downloads.location.directory = "~/downloads"
 
 c.fonts.default_family = "Droid Sans"
-
-c.content.headers.accept_language = "pt-BR,pt;q=0.9"
-c.content.cookies.store = False
 
 c.url.default_page = "https://duckduckgo.com/?k7=282a36&k8=f8f8f2&k9=50fa7b&kae=t&kt=p&ks=m&kw=n&km=l&ko=s&kj=282a36&ka=p&kaa=bd93f9&ku=-1&kx=f1fa8c&ky=44475a&kaf=1&kai=1&kf=1"
 c.url.start_pages = "https://start.duckduckgo.com/?k7=282a36&k8=f8f8f2&k9=50fa7b&kae=t&kt=p&ks=m&kw=n&km=l&ko=s&kj=282a36&ka=p&kaa=bd93f9&ku=-1&kx=f1fa8c&ky=44475a&kaf=1&kai=1&kf=1"
@@ -41,3 +48,7 @@ c.url.start_pages = "https://start.duckduckgo.com/?k7=282a36&k8=f8f8f2&k9=50fa7b
 c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}&k7=282a36&k8=f8f8f2&k9=50fa7b&kae=t&kt=p&ks=m&kw=n&km=l&ko=s&kj=282a36&ka=p&kaa=bd93f9&ku=-1&kx=f1fa8c&ky=44475a&kaf=1&kai=1&kf=1"
 }
+
+c.scrolling.bar = "always"
+
+c.tabs.background = True
